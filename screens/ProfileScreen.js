97e5335d09cc0b09e3d5f747e60fbf21eb525cf2
kahvecioglu <kahvecioglubugra@gmail.com>
2025-02-09 20:00:37 +0300

    Profile Screen fixed

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useFavorites } from '../compenents/FavoritesContext'; // Context'i kullanıyoruz
 import MovieCard from '../compenents/MovieCard';
@@ -12,6 +12,16 @@ const ProfileScreen = () => {
 
   return (
     <LinearGradient colors={['#a8c0ff', '#3f4c6b']} style={styles.container}>
+      {/* Kullanıcı profil fotoğrafı ve ismi */}
+      <View style={styles.profileHeader}>
+        <Image source={require('../assets/person.png')} style={styles.profileImage} />
+        <View style={styles.profileInfo}>
+          <Text style={styles.userName}>Buğra Kahvecioğlu</Text>
+          <Text style={styles.userEmail}>buğrakahvecioglu@example.com</Text>
+        </View>
+      </View>
+
+      {/* Favori Filmler Başlığı */}
       <Text style={styles.favoritesTitle}>Favori Filmlerim</Text>
 
       <FlatList
@@ -31,11 +41,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    padding: 20,
+    paddingTop: 40, // Ekranın üst kısmına padding ekledik
+    paddingHorizontal: 20,
+  },
+  profileHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 30, // Profil başlığından sonraya daha fazla boşluk ekledik
+  },
+  profileImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginRight: 20,
+  },
+  profileInfo: {
+    flexDirection: 'column',
+  },
+  userName: {
+    fontSize: 20,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  userEmail: {
+    fontSize: 16,
+    color: '#fff',
   },
   favoritesTitle: {
     fontSize: 24,
     color: '#fff',
+    marginTop:80,
+    marginBottom: 20, // Favori başlığından sonra biraz boşluk ekledik
   },
   emptyText: {
     fontSize: 18,
