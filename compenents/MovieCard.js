60c8bd3b0f55a18929ab29e18ddea25628aa20e1
kahvecioglu <kahvecioglubugra@gmail.com>
2025-02-09 18:04:08 +0300

    added AsyncStorage for favorite films

--- compenents/MovieCard.js
+++ compenents/MovieCard.js
@@ -84,6 +84,7 @@ const styles = StyleSheet.create({
     overflow: 'hidden',
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
+    
     shadowOpacity: 0.1,
     shadowRadius: 5,
     elevation: 5,
